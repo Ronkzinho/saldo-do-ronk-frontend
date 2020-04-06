019a9a05709f078bd47264689ae0d2fba8c5fb4f
Henrique Portes <hfrcr@hotmail.com>
2020-04-05 23:06:26 -0300

    Black mode add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import './App.css';
 import Routes from "./routes"
+import moon from "./assets/moon.png"
+import sun from "./assets/sun.png"
+import "./App.css"
+
 function App() {
+  var [theme, setTheme] = React.useState("")
+  React.useEffect(() => {
+    if(localStorage.getItem("theme") === "dark"){
+      document.body.classList.replace("light", "dark")
+      setTheme("dark")
+    }
+    else{
+      setTheme("light")
+    }
+  }, [theme])
+  function changeTheme(){
+      if(localStorage.getItem("theme") === "dark"){
+        document.body.classList.replace("dark", "light")
+        localStorage.setItem("theme", "light")
+        setTheme("light")
+      }
+      else{
+        document.body.classList.replace("light", "dark")
+        localStorage.setItem("theme", "dark")
+        setTheme("dark")
+      }
+  } 
   return (
     <div id="container" className="container">
+      <input className="theme-icon" type="image" alt="theme" onClick={() => { changeTheme() }} style={{ position: "absolute", right: "0", top: 1 }} width={36} height={36} src={(theme === "light") ? moon : sun }/>
       <Routes />
     </div>
   );
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import moment from 'moment/min/moment-with-locales'
 import Loading from "../../components/Loading"
 import api from "../../services/api"
+import "./styles.css"
 moment.locale("pt-br")
 
 export default function Dashboard({ history }){
@@ -19,6 +20,7 @@ export default function Dashboard({ history }){
         }
         load()
     }, [history])
+    
     async function validate(event){
         event.preventDefault()
         if(saldo === "" || isNaN(parseInt(saldo.replace(",", ".")))){
@@ -35,26 +37,31 @@ export default function Dashboard({ history }){
         setUserSaldo(res.data.saldo)
     }
     async function resetar(){
+        if(!window.confirm("Você tem certeza que deseja resetar seu saldo?")){
+            return
+        }
         var res = await api.delete("/dashboard", { headers: { _id: localStorage.getItem("user") }})
         if(res.data.error){
             return alert(res.data.error)
         }
         setUserSaldo(res.data.saldo)
     }
+    async function logout(){
+        localStorage.removeItem("user")
+        history.push("/")
+    }
     return(
         <>
         {(loaded === true) ?
 <> 
-        <button onClick={() => {
-        localStorage.removeItem("user")
-        history.push("/")
-        }}>Logout</button>
+        <button onClick={() => logout()}>Logout</button>
         <form onSubmit={validate}>
                 <h1 id="h2">Seu saldo:</h1>
-                <h2 id="saldo">{parseFloat(userSaldo).toFixed(2).replace(".", ",")}</h2>
-                <h3>Quanto?</h3>
+                <h2 id="saldo">R${parseFloat(userSaldo).toFixed(2).replace(".", ",")}</h2>
+                <h3>Deseja alterar o seu saldo?</h3>
                 <input type="number" placeholder="Coloque o valor aqui" value={saldo} onChange={(e) => setSaldo(e.target.value)}></input>
-                <h4>Pronto, agora para terminar, é so clicar em ir</h4>
+                <br/>
+                <br />
                 <button id="butao" type="submit">Ir</button>
                 <button onClick={resetar}>Resetar</button>
                 <button onClick={() => history.push("/history")}>Ir para o histórico</button>
